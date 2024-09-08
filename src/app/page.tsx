6f1e3ae35fccbf744019d598bba224c61e5d91fe
Hassan Kaeru <hassanaskary29@gmail.com>
2024-09-09 05:56:50 +0700

    add: carousel component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@ import { useEffect, useState } from "react";
 import Divider from "./components/Divider";
 import Popular from "./(home)/_components/Popular";
 import FlashSale from "./(home)/_components/Flashsale";
+import CarouselHomePage from "./(home)/_components/Carousel";
 
 export default function Home() {
   const [products, setProducts] = useState<string[]>([]);
   const [error, setError] = useState<string | null>(null);
-  const mockData = ["", "", "", "", "", "", "", ""];
+  // const mockData = ["", "", "", "", "", "", "", ""];
 
   const getProducts = async (): Promise<void> => {
     try {
@@ -43,7 +44,7 @@ export default function Home() {
   return (
     <div>
       <main className="flex flex-col">
-        <section>Carousel</section>
+        <CarouselHomePage/>
         <div className="flex flex-col gap-y-8 pt-8">
           <FlashSale />
           <Popular />
