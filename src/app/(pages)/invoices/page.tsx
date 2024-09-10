3d0c43ba84502552d27dcdb7aeca490b65719da0
Hassan Kaeru <hassanaskary29@gmail.com>
2024-09-10 21:12:19 +0700

    update:  invoice content

--- src/app/(pages)/invoices/page.tsx
+++ src/app/(pages)/invoices/page.tsx
@@ -1,7 +1,34 @@
+import Divider from "@/app/components/Divider";
+import { CgSearch } from "react-icons/cg";
+
 export default function Invoices() {
   return (
-    <div className="flex justify-center items-center h-screen w-screen">
-      Invoices Page
-    </div>
+    <>
+      <div className="container flex flex-col mx-auto px-4 py-12 space-y-5 xl:max-w-7xl justify-center items-center">
+        <div className="flex flex-col w-full gap-y-3">
+          <h1 className="font-bold text-3xl sm:text-4xl">Cari pesanan kamu!</h1>
+          <p>
+            Lacak transaksi kamu dengan cara memasukkan Nomor Invoice dibawah
+            ini:
+          </p>
+        </div>
+        <div className="flex flex-col w-full gap-y-2">
+          <h1 className="font-medium text-xs">Nomor Invoice Kamu</h1>
+          <input
+            id="invoice"
+            name="invoice"
+            placeholder="VGXXXXXXXXXXXXXX"
+            className=" max-w-[576px] px-3 py-2 placeholder-white text-xs text-white bg-[#535353] rounded-lg"
+          />
+        </div>
+        <div className="w-full flex justify-start font-medium text-sm">
+          <button className="flex items-center gap-2 pl-3 pr-4 py-2 bg-[#0563FC] hover:bg-[#0563FC]/75 active:scale-95 rounded-lg transition-all duration-300 ">
+            <CgSearch className=" h-5 w-5" />
+            <h1>Cari Transaksi</h1>
+          </button>
+        </div>
+      </div>
+      <Divider />
+    </>
   );
 }
