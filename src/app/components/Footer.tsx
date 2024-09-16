989feec9ea4cc9408edd34d10cf28944a0685eb7
Hassan Kaeru <hassanaskary29@gmail.com>
2024-09-16 23:46:07 +0700

    style: footer layout

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -9,7 +9,10 @@ const dataFooter = [
       { title: "Masuk", href: "/" },
       { title: "Daftar", href: "/" },
       { title: "Cek Transaksi", href: "/invoices" },
-      { title: "Hubungi Kami", href: "https://hassankary-portfolio.vercel.app/" },
+      {
+        title: "Hubungi Kami",
+        href: "https://hassankary-portfolio.vercel.app/",
+      },
       { title: "Ulasan", href: "https://www.vygaming.id/id/reviews" },
     ],
   },
@@ -31,22 +34,27 @@ export default function Footer() {
     <div className="w-full bg-[#121212]">
       <div className="container flex flex-col mx-auto px-4 py-12 space-y-16 xl:max-w-7xl items-center">
         <div className="flex flex-col w-full space-y-5">
-          <div className="w-full grid grid-cols-4 gap-8 font-semibold text-sm text-[#0563FC]">
-            {dataFooter.map((d) => {
-              return <div key={d.title}>{d.title}</div>;
-            })}
-          </div>
-          <div className="w-full grid grid-cols-4 gap-8 font-normal text-sm">
+          <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-8 font-normal text-sm">
             {dataFooter.map((d) => {
               return (
-                <div key={d.title} className="flex flex-col space-y-4">
-                  {d.data?.map((d) => {
-                    return (
-                      <Link key={d.title} href={d.href}>
-                        <div className="hover:text-[#0563FC]/75 transition-all">{d.title}</div>
-                      </Link>
-                    );
-                  })}
+                <div key={d.title} className="flex flex-col space-y-6">
+                  <div
+                    key={d.title}
+                    className="font-semibold text-[#0563FC] leading-6"
+                  >
+                    {d.title}
+                  </div>
+                  <div key={d.title} className="flex flex-col space-y-4">
+                    {d.data?.map((d) => {
+                      return (
+                        <Link key={d.title} href={d.href}>
+                          <div className="hover:text-[#0563FC]/75 transition-all leading-6">
+                            {d.title}
+                          </div>
+                        </Link>
+                      );
+                    })}
+                  </div>
                 </div>
               );
             })}
@@ -56,7 +64,12 @@ export default function Footer() {
           <h1>© 2024 VYGAMING STORE. All rights reserved.</h1>
           <h1>
             Built by{" "}
-            <Link href={"https://github.com/hassankary"} className=" font-semibold text-[#0563FC]">@hassankary</Link>
+            <Link
+              href={"https://github.com/hassankary"}
+              className=" font-semibold text-[#0563FC]"
+            >
+              @hassankary
+            </Link>
           </h1>
         </div>
       </div>
